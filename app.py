b7d8306d61f1ad2475ded326d17340903a4f9880
unknown <kostyapysmenny2@gmail.com>
2025-01-26 19:01:09 +0200

    update 2

--- app.py
+++ app.py
@@ -18,18 +18,12 @@ def get_data():
 def photo():
     date = request.form.get("date")
 
-    if not date:
-        return "Ви не ввели дату!", 400
-
-    try:
-        url = f"https://api.nasa.gov/planetary/apod?date={date}&api_key={API}"
-        result = requests.get(url=url)
-        if result.status_code == 400:
-            return render_template("error.html")
-        else:
-            return render_template("photo.html", photo=result.json()["hdurl"])
-    except ValueError:
-        return "Невірний формат дати!", 400
+    url = f"https://api.nasa.gov/planetary/apod?date={date}&api_key={API}"
+    result = requests.get(url=url)
+    if result.status_code == 400:
+        return render_template("error.html")
+    else:
+        return render_template("photo.html", photo=result.json()["hdurl"])
 
 
 if __name__ == "__main__":
